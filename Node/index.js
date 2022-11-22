8d3fb3b6db6cd5b4683a70e20145f6593b335c6d
vladislavshupilov <90390894+vladislavshupilov@users.noreply.github.com>
2022-11-22 14:34:18 +0200

    Add files via upload

---
+++ Node/index.js
@@ -0,0 +1,77 @@
+const obj = require ('./user')
+console.log(obj.user)
+obj.sayHello()
+console.log("work dir", __dirname)
+console.log("work file", __filename)
+
+
+
+const path = require('path')
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET') {
+        res.writaHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        })
+
+        if (req.url === '/') {
+            fs.readFile(
+                path.join(__dirname, 'views', 'index.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        throw err
+                    }
+
+                    res.end(content)
+                }
+            )
+        } else if (req.url === '/about') {
+            fs.readFile(
+                path.join(__dirname, 'views', 'about.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        throw err
+                    }
+
+                    res.end(content)
+                }
+            )
+        } else if (req.url === '/api/users') {
+            res.writeHead(200, {
+                'Content-Type': 'text/json'
+            })
+
+            const users = [
+                {name: 'Vasya', age: 20},
+                {name: 'Elena', age: 23}
+            ]
+
+            res.end(JSON.stringify(users))
+        }
+    }else if (req.method === 'POST') {
+        const body = []
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        })
+
+        req.on('data', data => {
+            dody.push(Buffer.from(data))
+        })
+
+        req.on('end', () => {
+            const message = body.toString().split('=')[1]
+
+            res.end(`
+                <h1>Ваше сообщение: ${message}</h1>
+            `)
+        })
+    }
+})
+
+server.listen(3000, () => {
+    console.log('Server is running...')
+})
---
+++ Node/user.js
@@ -0,0 +1,13 @@
+const user = {
+    name: 'Vasya',
+    age: 30
+}
+
+// module.exports = user
+
+module.exports = {
+    user: user,
+    sayHello() {
+        console.log("Hello!")
+    }
+}
